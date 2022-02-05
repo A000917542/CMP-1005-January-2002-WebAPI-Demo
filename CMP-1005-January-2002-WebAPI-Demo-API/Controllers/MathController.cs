73e47479348883da8c71f372f1333fc9c970c95b
Brent Ritchie <ritchie.brent@gmail.com>
2022-02-04 19:39:47 -0500

    Add project files.

---
+++ CMP-1005-January-2002-WebAPI-Demo-API/Controllers/MathController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMP_1005_January_2002_WebAPI_Demo_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        [HttpGet]
+        public double Add(int lValue, int rValue)
+        {
+            Response.Headers.Add("Access-Control-Allow-Origin", "https://localhost:44362");
+            Response.Headers.Add("Access-Control-Allow-Origin", "https://ca.yahoo.com");
+            return lValue + rValue;
+        }
+
+        [HttpGet]
+        public double Subtract(int lValue, int rValue)
+        {
+            return lValue - rValue;
+        }
+    }
+}
---
+++ CMP-1005-January-2002-WebAPI-Demo-API/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CMP_1005_January_2002_WebAPI_Demo_API
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
